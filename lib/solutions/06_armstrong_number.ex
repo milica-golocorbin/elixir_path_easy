303fd76843719bbf3a742053a779d269884def3d
milicagolocorbin <milicagolocorbin@gmail.com>
2023-02-20 09:42:33 +0100

    armstrong number solutions

---
+++ lib/solutions/06_armstrong_number.ex
@@ -0,0 +1,16 @@
+defmodule Solutions.ArmstrongNumber do
+  @moduledoc """
+  Provides a way to validate whether or not a number is an Armstrong number
+  """
+
+  @spec valid?(integer) :: boolean
+  def valid?(number) do
+    numbers_list = Integer.digits(number)
+    exponent = length(numbers_list)
+
+    number ==
+      Enum.map(numbers_list, fn number -> Integer.pow(number, exponent) end)
+      |> Enum.sum()
+      |> IO.inspect()
+  end
+end
